36dfcb335fe0e30708659333f9663e24f90093ec
Shankari Ramanan <shankari@Shankaris-MacBook-Pro.local>
2023-09-19 16:05:01 -0400

    first commit

---
+++ src/main/java/com/ram/spring/application/Application.java
@@ -0,0 +1,11 @@
+package com.ram.spring.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+    }
+}
---
+++ src/main/kotlin/com/ram/spring/application/MyObjects.kt
@@ -0,0 +1,4 @@
+package com.ram.spring.application
+
+class MyObjects {
+}
